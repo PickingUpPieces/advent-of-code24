1e623e0ed77fce77d55cdf288e3465d12442796b
Vincent Picking <v.picking@gmx.de>
2024-12-05 23:15:18 +0100

    Add day05

---
+++ src/day05/bin/main.rs
@@ -0,0 +1,11 @@
+use log::{debug, info};
+use advent_of_code::helpers;
+
+// Words allowed to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. 
+// Find all occurences of the word "XMAS"
+
+fn main() {
+    helpers::init();
+    info!("Start day 5 challenge...");
+
+}

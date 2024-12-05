b4f34c31012edcb8bf2d4b5444751ba7a0f49efd
Vincent Picking <v.picking@gmx.de>
2024-12-05 17:55:39 +0100

    Add day3b

--- src/day03/bin/main.rs
+++ src/day03/bin/main.rs
@@ -12,18 +12,46 @@ fn main() {
     let input = include_str!("../input.txt");
     let match_mul = Regex::new(r"mul\(\d+,\d+\)").unwrap();
     let all_multiply_operations: Vec<&str> = match_mul.find_iter(&input).map(|occ| occ.as_str()).collect();
-
     debug!("Multiplication found: {:?}", all_multiply_operations);
 
-    let match_numbers = Regex::new(r"(\d+),(\d+)").unwrap(); 
-
+    // Iterate over multiplications
     let result = all_multiply_operations.iter().fold(0, |acc, mul| {
-        acc + if let Some(matches) = match_numbers.captures(mul) {
-            matches[1].parse::<usize>().unwrap() * matches[2].parse::<usize>().unwrap()
-        } else {
-            0
-        }
+        acc + exec_multiply(mul)
     });
 
     info!("The result is: {}", result);
+
+    info!("Start day 3b challenge...");
+
+    // Parse input
+    let input = include_str!("../input.txt");
+    let match_mul = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
+    let all_multiply_operations: Vec<&str> = match_mul.find_iter(&input).map(|occ| occ.as_str()).collect();
+    debug!("Overall commands found: {:?}", all_multiply_operations);
+
+    let mut is_enabled: bool = true;
+    let mut result = 0;
+
+    for instruction in all_multiply_operations {
+        result += match instruction.split('(').next().unwrap() {
+            "mul" if is_enabled => exec_multiply(instruction),
+            "mul" => 0,
+            "do" => { is_enabled = true; 0 },
+            "don't" => { is_enabled = false; 0 },
+            _ => panic!("Received unkown instruction")
+        };
+    }
+
+    info!("The result is: {}", result);
+}
+
+
+fn exec_multiply(mul: &str) -> usize {
+    let match_numbers = Regex::new(r"(\d+),(\d+)").unwrap(); 
+
+    if let Some(matches) = match_numbers.captures(mul) {
+        matches[1].parse::<usize>().unwrap() * matches[2].parse::<usize>().unwrap()
+    } else {
+        0
+    }
 }

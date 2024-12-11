bfc821e156aac3f79d3cb41ade182ee416f56ba1
Vincent Picking <v.picking@gmx.de>
2024-12-11 12:28:05 +0100

    Restructure day5

--- src/day05/bin/main.rs
+++ src/day05/bin/main.rs
@@ -8,8 +8,76 @@ fn main() {
     helpers::init();
     info!("Start day 5 challenge...");
 
-    let mut rules: HashMap<usize, Vec<usize>> = HashMap::new();
+    let (unsafe_reports, result) = part_one();
+    info!("Result of PART ONE: {result}");
+
+    info!("Amount of unsafe updates: {}", unsafe_reports.len());
+    info!("PART TWO: Corrected unsafe reports result: {}", part_two(unsafe_reports));
+}
+
+fn part_one() -> (Vec<Vec<usize>>, usize) {
+    let (updates, rules) = parse();
+    let mut result: usize = 0;
+    let mut unsafe_reports: Vec<Vec<usize>> = Vec::new();
+
+    'outer: for update in updates {
+        let mut not_allowed_pages: HashSet<usize> = HashSet::new();
+
+        for page in &update {
+            if not_allowed_pages.contains(page) {
+                debug!("Update is unsafe: {:?}", update);
+                unsafe_reports.push(update.clone());
+                continue 'outer;
+            }
+            not_allowed_pages.extend(rules.get(page).cloned().unwrap_or_default());
+        }
+
+        // Correct the calculation of the middle element
+        let middle_index = update.len() / 2;
+        result += update[middle_index];
+    }
+    (unsafe_reports, result)
+}
+
+fn part_two(unsafe_reports: Vec<Vec<usize>>) -> usize {
+    let (_, rules) = parse();
+    let mut result: usize = 0;
+
+    // Part 2 looks like topological sort, but just bruting it
+    for mut update in unsafe_reports {
+        loop {
+            if let Some(value) = check_update(&mut update, &rules) {
+                result += value;
+                break;
+            }
+        }
+    }
+    result
+}
+
+
+fn check_update(update: &mut Vec<usize>, rules: &HashMap<usize, Vec<usize>>) -> Option<usize> {
+    let mut not_allowed_pages: HashSet<usize> = HashSet::new();
+
+    for (index, page) in update.iter().enumerate() {
+        // If hitting unsafe report, swap the two entries in the vector and continue
+        if not_allowed_pages.contains(&page) {
+            debug!("Update is unsafe on page {page}: {:?}", update);
+            // search vector for clashing element and swap with current index
+            let index_of_clashing_element = update[0..index].iter().position(|&e| rules.get(&e).cloned().unwrap_or_default().contains(&page)).unwrap();
+            debug!("Swap clashing element {} with {}", update[index_of_clashing_element], update[index]);
+            update.swap(index, index_of_clashing_element);
+            return None;
+        } 
+        not_allowed_pages.extend(rules.get(page).cloned().unwrap_or_default());
+    }
+
+    update.get(update.len() / 2).copied()
+}
+
+fn parse() -> (Vec<Vec<usize>>, HashMap<usize, Vec<usize>>) {
     let mut updates: Vec<Vec<usize>> = Vec::new();
+    let mut rules: HashMap<usize, Vec<usize>> = HashMap::new();
     let mut is_updates = false;
 
     for line in include_bytes!("../input.txt").split(|&byte| byte == b'\n') {
@@ -26,63 +94,5 @@ fn main() {
             rules.entry(atoi::atoi::<usize>(&line[3..5]).unwrap()).or_default().push(value);
         }
     }
-
-    debug!("Hashmap: {:?}", rules);
-    debug!("Updates: {:?}", updates);
-    let mut result: usize = 0;
-    let mut unsafe_reports: Vec<Vec<usize>> = Vec::new();
-
-    //  1. Check if value is in not_allowed_pages: Here are the pages included which we should not see in the update implied by the given rules
-    //  2. Get current page from hashmap
-    //  3. Store all pages we should not see in the rest of the update in the not_allowed_pages set
-    //  4. Get the middle element, if update is safe
-    'outer: for update in updates {
-        let mut not_allowed_pages: HashSet<usize> = HashSet::new();
-
-        for page in update.clone() {
-            if not_allowed_pages.contains(&page) {
-                debug!("Update is unsafe: {:?}", update);
-                unsafe_reports.push(update);
-                continue 'outer;
-            }
-            not_allowed_pages.extend(rules.get(&page).cloned().unwrap_or_default());
-        }
-
-        result += update.get(update.len().div_ceil(2)).unwrap_or(&0);
-    }
-    info!("The result is: {}", result);
-
-    // Part 2 looks like topological sort, but just bruting it
-    info!("Amount of unsafe updates: {}", unsafe_reports.len());
-    result = 0;
-
-    for mut update in unsafe_reports {
-        loop {
-            if let Some(value) = check_update(&mut update, &rules) {
-                result += value;
-                break;
-            }
-        }
-    }
-    info!("Corrected unsafe reports result: {}", result);
-}
-
-
-fn check_update(update: &mut Vec<usize>, rules: &HashMap<usize, Vec<usize>>) -> Option<usize> {
-    let mut not_allowed_pages: HashSet<usize> = HashSet::new();
-
-    for (index, page) in update.iter().enumerate() {
-        // If hitting unsafe report, swap the two entries in the vector and continue
-        if not_allowed_pages.contains(&page) {
-            info!("Update is unsafe on page {page}: {:?}", update);
-            // search vector for clashing element and swap with current index
-            let index_of_clashing_element = update[0..index].iter().position(|&e| rules.get(&e).cloned().unwrap_or_default().contains(&page)).unwrap();
-            info!("Swap clashing element {} with {}", update[index_of_clashing_element], update[index]);
-            update.swap(index, index_of_clashing_element);
-            return None;
-        } 
-        not_allowed_pages.extend(rules.get(page).cloned().unwrap_or_default());
-    }
-
-    update.get(update.len() / 2).copied()
+    (updates, rules)
 }

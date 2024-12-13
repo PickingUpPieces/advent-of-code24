a5f44a7c887025d591f6b2b0d362c8095cd25ff1
Vincent Picking <v.picking@gmx.de>
2024-12-13 13:41:24 +0100

    Add day13

---
+++ src/day13/bin/main.rs
@@ -0,0 +1,22 @@
+use advent_of_code::helpers;
+use log::info;
+
+fn main() {
+    helpers::init();
+    info!("Start day 13 challenge...");
+
+    info!("Solution for PART ONE is {}", part_one());
+    info!("Solution for PART TWO is {}", part_two());
+}
+
+fn part_one() -> usize {
+    0
+}
+
+fn part_two() -> usize {
+    0
+}
+
+fn parse() {
+
+}
